e6463553838b8a0c3d563a61945d35ec3e4bfdff
Shahriar <mk.shahariar@gmail.com>
2018-07-29 03:29:20 +0600

    cf 938e solved

---
+++ CodeForces/CF938-D12-E.cpp
@@ -0,0 +1,239 @@
+#include <cstdio>
+#include <sstream>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <stack>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <iterator>
+//#include <bits/stdc++.h>
+
+using namespace std;
+
+/// Policy Based DS
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+/// Policy Based DS
+
+#define HI              printf("HI\n")
+#define sf              scanf
+#define pf              printf
+#define sf1(a)          scanf("%d",&a)
+#define sf2(a,b)        scanf("%d %d",&a,&b)
+#define sf3(a,b,c)      scanf("%d %d %d",&a,&b,&c)
+#define sf4(a,b,c,d)    scanf("%d %d %d %d",&a,&b,&c,&d)
+#define sf1ll(a)        scanf("%lld",&a)
+#define sf2ll(a,b)      scanf("%lld %lld",&a,&b)
+#define sf3ll(a,b,c)    scanf("%lld %lld %lld",&a,&b,&c)
+#define sf4ll(a,b,c,d)    scanf("%lld %lld %lld %lld",&a,&b,&c,&d)
+#define pb              push_back
+#define ppb             pop_back
+#define ppf             push_front
+#define popf            pop_front
+#define ll              long long int
+#define ui              unsigned int
+#define ull             unsigned long long
+#define fs              first
+#define sc              second
+#define clr( a, b )     memset((a),b,sizeof(a))
+#define pii            pair<int, int>
+#define pll         pair<long long int, long long int>
+#define mp              make_pair
+#define max3(a,b,c)     max(a,max(b,c))
+#define min3(a,b,c)     min(a,min(b,c))
+#define PI              acos(-1.0)
+#define wait            system("pause")
+#define ps              pf("PASS\n")
+
+template<class T1> void deb(T1 e1)
+{
+    cout<<e1<<endl;
+}
+template<class T1,class T2> void deb(T1 e1,T2 e2)
+{
+    cout<<e1<<" "<<e2<<endl;
+}
+template<class T1,class T2,class T3> void deb(T1 e1,T2 e2,T3 e3)
+{
+    cout<<e1<<" "<<e2<<" "<<e3<<endl;
+}
+template<class T1,class T2,class T3,class T4> void deb(T1 e1,T2 e2,T3 e3,T4 e4)
+{
+    cout<<e1<<" "<<e2<<" "<<e3<<" "<<e4<<endl;
+}
+template<class T1,class T2,class T3,class T4,class T5> void deb(T1 e1,T2 e2,T3 e3,T4 e4,T5 e5)
+{
+    cout<<e1<<" "<<e2<<" "<<e3<<" "<<e4<<" "<<e5<<endl;
+}
+template<class T1,class T2,class T3,class T4,class T5,class T6> void deb(T1 e1,T2 e2,T3 e3,T4 e4,T5 e5,T6 e6)
+{
+    cout<<e1<<" "<<e2<<" "<<e3<<" "<<e4<<" "<<e5<<" "<<e6<<endl;
+}
+
+/// <---------------------------  For Bitmasking  -------------------------------->
+//int on( int n, int pos ){
+//    return n = n|( 1<<pos );
+//}
+//bool check( int n, int pos ){
+//    return (bool)( n&( 1<<pos ) );
+//}
+//int off( int n, int pos ){
+//    return n = n&~( 1<<pos );
+//}
+//int toggle( int n, int pos ){
+//    return n = n^(1<<pos);
+//}
+//int count_bit( int n ){
+//    return __builtin_popcount( n );
+//}
+/// <---------------------------  End of Bitmasking  -------------------------------->
+
+
+/// <--------------------------- For B - Base Number System ----------------------------------->
+//int base;
+//int pw[10];
+//void calPow(int b){
+//    base = b;
+//    pw[0] = 1;
+//    for( int i = 1; i<10; i++ ){
+//        pw[i] = pw[i-1]*base;
+//    }
+//}
+//int getV(int mask, int pos){
+//    mask /= pw[pos];
+//    return ( mask%base );
+//}
+//int setV(int mask, int v, int pos){
+//    int rem = mask%pw[pos];
+//    mask /= pw[pos+1];
+//    mask = ( mask*base ) + v;
+//    mask = ( mask*pw[pos] ) + rem;
+//    return mask;
+//}
+/// <--------------------------- End B - Base Number System ----------------------------------->
+
+
+// moves
+
+//int dx[]= {0,0,1,-1};/*4 side move*/
+//int dy[]= {-1,1,0,0};/*4 side move*/
+//int dx[]= {1,1,0,-1,-1,-1,0,1};/*8 side move*/
+//int dy[]= {0,1,1,1,0,-1,-1,-1};/*8 side move*/
+//int dx[]={1,1,2,2,-1,-1,-2,-2};/*night move*/
+//int dy[]={2,-2,1,-1,2,-2,1,-1};/*night move*/
+
+//double Expo(double n, int p) {
+//	if (p == 0)return 1;
+//	double x = Expo(n, p >> 1);
+//	x = (x * x);
+//	return ((p & 1) ? (x * n) : x);
+//}
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> set_t;
+
+pll extended_euclid(ll a, ll b){ // returns x, y | ax + by = gcd(a,b)
+    if(b == 0)    return pll(1, 0);
+    else{
+        pll d = extended_euclid(b, a%b);
+        return pll(d.second, d.first - d.second*(a/b));
+    }
+}
+ll modular_inverse(ll a, ll m) {
+    pll ret = extended_euclid(a, m);
+    return ((ret.first%m)+m)%m;
+}
+
+#define MXN
+#define MXE
+#define MXQ
+#define SZE
+#define MOD 1000000007
+#define MX 1000000
+#define EPS
+#define INF 1000000009
+
+const int mod = 1000000007;
+
+int n, arr[MX+7];
+
+int main(){
+//    freopen("H:\\00.txt", "r", stdin);
+    int i, j, k;
+
+    sf("%d", &n);
+    for( i = 0; i<n; i++ )
+        sf("%d", &arr[i]);
+    sort(arr, arr+n);
+
+    int cnt = 0;
+    ll ans = 0;
+
+    for( i = 0; i<n; i++ ){
+        if( arr[i] == arr[n-1] )
+            break;
+        if( i>0 and arr[i-1]!=arr[i] )
+            cnt = i;
+        ans += ( (ll)arr[i]*modular_inverse(n-cnt, mod) )%mod;
+        ans %= mod;
+    }
+    for( i = 2; i<=n; i++ )
+        ans = ( ans*(ll)i )%mod;
+    pf("%lld\n", ans);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
